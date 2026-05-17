9dd09438c8757a6ff227c32b3daeab5901814434
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-05-17 22:37:08 +0300

    [ssh] Run ssh-agent as a wrapper program to enable integration with the default keyring

--- .config/zsh/conf.d/01-environment.zsh
+++ .config/zsh/conf.d/01-environment.zsh
@@ -3,7 +3,6 @@ export EDITOR=nvim
 export VISUAL=nvim
 
 # <https://wiki.archlinux.org/title/GnuPG#Configure_pinentry_to_use_the_correct_TTY>
-# TODO: Move into separate file.
 export GPG_TTY="$(tty)"
 
 # Change the delay of pressing Esc key in `vi` mode.
@@ -20,15 +19,3 @@ export LESSOPEN="| /usr/bin/source-highlight-esc.sh %s"
 export GOPATH="${XDG_DATA_HOME}/go"
 export VAGRANT_HOME="${XDG_DATA_HOME}/vagrant"
 export XCURSOR_PATH="/usr/share/icons"
-
-# X11 settings I configured previously but
-# don't recall the purpose.
-# Keeping them here for future reference.
-#
-# export LIBVA_DRIVER_NAME=vdpau
-# export VDPAU_DRIVER=nvidia
-# export XDG_SESSION_TYPE=x11
-# export QT_QPA_PLATFORMTHEME=gtk2
-# export DESKTOP_SESSION=gnome
-# export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
-# export SAL_USE_VCLPLUGIN=gtk3
