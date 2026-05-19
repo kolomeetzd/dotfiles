3a901c0d1c2ae7427907b93c87a55a18cd697996
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-05-19 15:36:33 +0300

    [XDG] Run user-specific executable files from `$HOME/.local/bin`

--- .config/zsh/conf.d/06-keybinds.zsh
+++ .config/zsh/conf.d/06-keybinds.zsh
@@ -10,22 +10,23 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey '^P' up-line-or-beginning-search
-bindkey '^N' down-line-or-beginning-search
-bindkey '^A' vi-beginning-of-line
-bindkey '^E' vi-end-of-line
+bindkey "^P" up-line-or-beginning-search
+bindkey "^N" down-line-or-beginning-search
+bindkey "^A" vi-beginning-of-line
+bindkey "^E" vi-end-of-line
 
-## Fix Ctrl+Right/Left.
+# Fix Ctrl+Right/Left.
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 
-bindkey '^h' backward-delete-char
-bindkey '^w' backward-kill-word
+bindkey "^h" backward-delete-char
+bindkey "^w" backward-kill-word
 # Conflicts with <https://wiki.archlinux.org/title/Fzf#Zsh>.
-# bindkey '^r' history-incremental-search-backward
+# bindkey "^r" history-incremental-search-backward
 
-bindkey -s '^[f' '~/.local/bin/tmux-sessionizer^M'
-# bindkey -s '^[d' '~/.local/share/scripts/todo.sh^M'
-bindkey -s '^[s' '~/.local/bin/tmux-sessionizer ~/.local/share/scripts/^M'
-bindkey -s '^[t' '~/.local/bin/tmux-sessionizer ~/.config/tmux/^M'
-bindkey -s '^[v' '~/.local/bin/tmux-sessionizer ~/.config/nvim/^M'
+# Tmux sessionizer defaults.
+bindkey -s "^[f" "$HOME/.local/bin/tmux-sessionizer^M"
+bindkey -s "^[t" "$HOME/.local/bin/tmux-sessionizer $XDG_CONFIG_HOME/nvim^M"
+bindkey -s "^[v" "$HOME/.local/bin/tmux-sessionizer $XDG_CONFIG_HOME/tmux^M"
+
+# bindkey -s "^[d" "$HOME/.local/bin/todo.sh^M"
