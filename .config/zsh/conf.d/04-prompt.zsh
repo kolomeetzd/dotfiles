82f2eef0fc892a28eb1cd98da5f935371aaa47bf
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-05-21 23:55:18 +0300

    [zsh] Refactor prompt theme

--- .config/zsh/conf.d/04-prompt.zsh
+++ .config/zsh/conf.d/04-prompt.zsh
@@ -2,85 +2,146 @@
 
 # 04-prompt.zsh - Configures a custom prompt.
 
-# Prompt initialization.
+# reset_color='%f%k%b'
+
+# 1. Prompt system init
 autoload -Uz promptinit
 promptinit
 
-prompt off
+# 2. Global options
+
+# Enables prompt substitution.
 setopt prompt_subst
 
-# Colorize the prompt char depending of user.
-function _prompt_char() {
-  if [[ $EUID -eq 0 ]]; then
-    echo '%F{9}%#%f'
-  else
-    echo '%F{10}%#%f'
-  fi
-}
+# 3. Helper functions
 
-# Get the current directory length and reduce it to <= 45.
-function _current_directory() {
-  local _max_pwd_length="45"
-  if [[ $(echo -n $PWD | wc -c) -gt ${_max_pwd_length} ]]; then
-    echo '%-2~/../%1~'
-  else
-    echo '%~'
-  fi
-}
-
-# Show Git status.
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' stagedstr '%F{2} ●%f'
-zstyle ':vcs_info:*' unstagedstr '%F{yellow} ●%f'
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{red}:%f%F{yellow}%r%f'
-zstyle ':vcs_info:*' enable git
-
-precmd () {
-    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-        zstyle ':vcs_info:*' formats '%F{12}/%b%c%u%f%F{12}%f'
-    } else {
-        zstyle ':vcs_info:*' formats '%F{12}/%b%c%u%f%F{red} ●%f%F{12}%f'
-    }
-    vcs_info
-}
-
-# Show current vi mode.
-function vi_mode_prompt_info() {
-  VIM_PROMPT="%F{8}✗ n %f"
-  VIM_RPROMPT="%F{8}→ i %f"
-
-  echo "${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/$VIM_RPROMPT}"
-}
-
-# Show if virtual environment is being used.
-function venv_info {
-  [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
-}
-
-# Helps format RPROMPT.
+# Colorizes username (%n) based on $EUID.
 function _current_user() {
-  if [[ $(echo "$USER") != "root" ]]; then
-    echo "%n";
-  else
-    echo "%f%F{1}%n%f%F{8}";
-  fi
+  [[ $EUID -eq 0 ]] && print - '%F{1}%n%f' || print - '%F{8}%n%f'
 }
 
-
-# Format custom prompt.
-function zle-line-init zle-keymap-select {
-  PROMPT='%{%f%k%b%}
- %F{8}$(_current_directory)%f ${vcs_info_msg_0_}
- $(venv_info)$(_prompt_char) '
- RPROMPT='%{$(echotc UP 1)%}$(vi_mode_prompt_info)%F{8}/%l:$(_current_user)@%M%f%{$(echotc DO 1)%}'
-
-  zle reset-prompt
-  zle -R
+# [user@host]
+function _user_host() {
+    print - '$(_current_user)%F{8}@%m%f'
 }
 
-# Other settings.
-zle -N zle-line-init
-zle -N zle-keymap-select
+function _line_tty() {
+    print - '%F{8}/%y%f'
+}
 
-DISABLE_UPDATE_PROMPT=true
+# Truncates current directory to ~/../dir if $PWD > 45 chars.
+function _current_dir() {
+    local pwd_s='%~'
+
+    [[ ${#PWD} -gt 45 ]] && pwd_s='%-2~/../%1~'
+
+    print -P '%F{8}$pwd_s%f'
+}
+
+# Colorizes prompt symbol based on privileges.
+function _prompt_char() {
+    [[ $EUID -eq 0 ]] && print - '%F{1}%#%f' || print - '%F{2}%#%f'
+}
+
+# Show if python virtual environment is being used.
+function _venv_info {
+    print -P '%F{6}[venv:$(basename $VIRTUAL_ENV)]%f'
+}
+
+# 4. Git integration: vcs_info + styles
+
+autoload -Uz vcs_info
+
+# Enables git integration for vcs_info.
+#
+# NOTE: Not required — git support is enabled by default.
+#       Keep this commented line as a reference for future use.
+# zstyle ':vcs_info:*' enable git
+
+# # Formats the VCS prompt for SVN, SVK, and BZR.
+#
+# NOTE: Not required — I use only git.
+#       Keep this line commented as a reference for future use.
+# zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{red}:%f%F{yellow}%r%f'
+
+# Enables scanning for changes in the git repository.
+zstyle ':vcs_info:git:*' check-for-changes true
+
+# Fetches current revision identifier (commit hash for git).
+# Makes %i available in formats/actionformats.
+# Small performance cost.
+# NOTE: Not required - we don't use %i in the prompt message.
+#       Keep this line commented as a reference for future use.
+# zstyle ':vcs_info:*' get-revision true
+
+# Explicitly enables check for staged changes.
+# Often redundant when `check-for-changes true` is set,
+# but makes staged detection (%c) more reliable in some backends.
+# NOTE: Not required - `check-for-check` is already set.
+#       Keep this line commented as a reference for future use.
+# zstyle ':vcs_info:*' check-for-staged-changes true
+
+# Displays git status in normal state.
+# %b - branch name, %c - staged changes marker, %u - unstaged changes marker.
+zstyle ':vcs_info:git:*' formats '%F{4}/%b%f%c%u' # Blue text for branch name.
+# Displays git status during special operations (e.g., rebase, merge, bisect).
+# %a - current action being performed.
+zstyle ':vcs_info:git:*' actionformats '%F{4}/%b%f::%F{6}a%%fc%u%f' # Cyan for action.
+
+# Defines marker styles for changes in the git repository.
+zstyle ':vcs_info:git:*' stagedstr   '%F{2} ●%f'  # Green dot for staged changes.
+zstyle ':vcs_info:git:*' unstagedstr '%F{3} ●%f'  # Yellow dot for unstaged changes.
+
+# Shows marker if there are untracked files in repository.
+# NOTE: Make sure you have added unstaged (%u) to your 'formats'.
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+        # This will show the marker if there are any untracked files in repo.
+        git status --porcelain | grep -q '^?? ' 2> /dev/null ; then
+        # If instead you want to show the marker only if there are untracked
+        # files in $PWD, use:
+        #[[ -n $(git ls-files --others --exclude-standard) ]] ; then
+        hook_com[unstaged]+='%F{1} ●%f' # Red dot for untracked files.
+    fi
+}
+
+# NOTE: vcs_info also able to:
+#       - Compare local changes to remote changes (+N/-N).
+#       - Show remote branch name for remote-tracking branches.
+#       - And more.
+#       - See: <https://github.com/zsh-users/zsh/blob/master/Misc/vcs_info-examples>
+
+# 5. precmd function
+
+# Use precmd hook to rebuild PROMPT dynamically on every prompt.
+# Static assignment would freeze $( ) expansions at setup time.
+# Adds vcs_info functionality to existing setups.
+function _thewayilikeit_precmd() {
+    vcs_info
+
+    [[ -n $VIRTUAL_ENV ]] && PROMPT="$(_current_dir) $(_venv_info)" || PROMPT="$(_current_dir)"
+    [[ -n ${vcs_info_msg_0_} ]] && PROMPT+=" ${vcs_info_msg_0_}"
+
+    PROMPT+=$'\n'
+    PROMPT+="$(_prompt_char) "
+
+    RPROMPT="$(_line_tty)%F{8}:%f$(_user_host)"
+}
+
+# 6. Theme setup
+
+prompt_thewayilikeit_setup() {
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd _thewayilikeit_precmd
+}
+
+# 7. Register and load theme
+
+# Add the theme to promptsys.
+prompt_themes+=( thewayilikeit )
+typeset -U prompt_themes
+
+# Load it.
+prompt thewayilikeit
