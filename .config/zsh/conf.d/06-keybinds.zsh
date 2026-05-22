eeb80bfdcde3a73efe961ba5ca504e5706adc985
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-05-22 23:12:29 +0300

    refactor(zsh): use string capabilities from terminfo in key bindings

--- .config/zsh/conf.d/06-keybinds.zsh
+++ .config/zsh/conf.d/06-keybinds.zsh
@@ -1,32 +1,75 @@
 #!/usr/bin/env zsh
 
-# 06-keybinds.zsh - Manages shell keybindings.
+# file: $ZDOTDIR/conf.d/06-keybinds.zsh
 #
-# TODO: Refactor using <https://wiki.archlinux.org/title/Zsh#Key_bindings>.
+# Manages key bindings in Zsh is by using string capabilities from terminfo.
+# See: <https://wiki.archlinux.org/title/Zsh#Key_bindings>.
 
-bindkey -v
+# Create a zkbd compatible hash.
+typeset -g -A key
 
+# To add other keys to this hash,
+# see: man 5 terminfo - <https://man.archlinux.org/man/terminfo.5>.
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Insert]="${terminfo[kich1]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+
+# Setup key accordingly.
+[[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
+[[ -n "${key[End]}"       ]] && bindkey -- "${key[End]}"        end-of-line
+[[ -n "${key[Insert]}"    ]] && bindkey -- "${key[Insert]}"     overwrite-mode
+[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}"  backward-delete-char
+[[ -n "${key[Delete]}"    ]] && bindkey -- "${key[Delete]}"     delete-char
+[[ -n "${key[Up]}"        ]] && bindkey -- "${key[Up]}"         up-line-or-history
+[[ -n "${key[Down]}"      ]] && bindkey -- "${key[Down]}"       down-line-or-history
+[[ -n "${key[Left]}"      ]] && bindkey -- "${key[Left]}"       backward-char
+[[ -n "${key[Right]}"     ]] && bindkey -- "${key[Right]}"      forward-char
+[[ -n "${key[PageUp]}"    ]] && bindkey -- "${key[PageUp]}"     beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}"  ]] && bindkey -- "${key[PageDown]}"   end-of-buffer-or-history
+[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}"  reverse-menu-complete
+
+# Use Ctrl+Left/Right to move to the beginning of the previous/next word.
+key[Control-Left]="${terminfo[kLFT5]}"
+key[Control-Right]="${terminfo[kRIT5]}"
+
+[[ -n "${key[Control-Left]}"  ]] && bindkey -- "${key[Control-Left]}"  backward-word
+[[ -n "${key[Control-Right]}" ]] && bindkey -- "${key[Control-Right]}" forward-word
+
+# Enable history search.
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey "^P" up-line-or-beginning-search
-bindkey "^N" down-line-or-beginning-search
-bindkey "^A" vi-beginning-of-line
-bindkey "^E" vi-end-of-line
+bindkey -- "^P" up-line-or-beginning-search
+bindkey -- "^N" down-line-or-beginning-search
 
-# Fix Ctrl+Right/Left.
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-
-bindkey "^h" backward-delete-char
-bindkey "^w" backward-kill-word
-# Conflicts with <https://wiki.archlinux.org/title/Fzf#Zsh>.
-# bindkey "^r" history-incremental-search-backward
+# Move cursor to the beginning/end of the line (emacs-like).
+bindkey -- "^A" beginning-of-line
+bindkey -- "^E" end-of-line
 
 # Tmux sessionizer defaults.
 bindkey -s "^[f" "$HOME/.local/bin/zsesh^M"
 bindkey -s "^[t" "$HOME/.local/bin/zsesh $XDG_CONFIG_HOME/nvim^M"
 bindkey -s "^[v" "$HOME/.local/bin/zsesh $XDG_CONFIG_HOME/tmux^M"
-
 # bindkey -s "^[d" "$HOME/.local/bin/todo.sh^M"
+
+# Checks wether the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+    autoload -Uz add-zle-hook-widget
+
+    function zle_application_mode_start { echoti smkx }
+    function zle_application_mode_stop { echoti rmkx }
+
+    add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+    add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
