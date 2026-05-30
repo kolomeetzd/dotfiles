1f4ed1efb00a4f31470b48c37c7c8d6ef6bef098
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-05-30 19:39:30 +0300

    config(redshift) adjust brightness

--- .config/redshift/hooks/brightness.sh
+++ .config/redshift/hooks/brightness.sh
@@ -4,9 +4,9 @@
 
 # Set brightness values for each status.
 # Range from 1 to 100 is valid
-brightness_day=30
-brightness_transition=25
-brightness_night=20
+brightness_day=20
+brightness_transition=15
+brightness_night=10
 
 if [ "$1" = period-changed ]; then
 	case $3 in
